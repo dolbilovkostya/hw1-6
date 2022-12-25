d9c9c07b44f6c022cc41f9ca8bfe4fd16a54b36f
dolbilov <dolbilovkonstantin@gmail.com>
2022-12-25 12:49:35 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    order = db.relationship("Order")
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor = db.relationship("User")
+
+
+db.create_all()
+db.session.commit()
---
+++ models.py
@@ -0,0 +1,70 @@
+from config import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    email = db.Column(db.String)
+    role = db.Column(db.String)
+    phone = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age,
+            'email': self.email,
+            'role': self.role,
+            'phone': self.phone
+        }
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    start_date = db.Column(db.Integer)
+    end_date = db.Column(db.Integer)
+    address = db.Column(db.String)
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    customer = db.relationship("User")
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor = db.relationship("User")
+
+    def get_order(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'address': self.address,
+            'price': self.price,
+            'customer_id': self.customer_id,
+            'executor_id': self.executor_id
+        }
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    order = db.relationship("Order")
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor = db.relationship("User")
+
+    def get_offer(self):
+        return {
+            'id': self.id,
+            'order_id': self.order_id,
+            'executor_id': self.executor_id
+        }
+    
+    
+ 
